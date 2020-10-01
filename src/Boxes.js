20a59e6c0e34c10bdde6ddb63e35475f1c6b3c9e
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-10-01 13:29:05 -0500

    edited boxes.js

--- src/Boxes.js
+++ src/Boxes.js
@@ -8,16 +8,40 @@ export default class Boxes extends React.Component {
         return (
             <View style={styles.container}>
 
+                <View style={styles.box}>
+                    <View style={styles.inner}>
+                        <Text>Box 1</Text>
+                    </View>
+                </Vew>
+
+                <View style={styles.box}>
+                    <View style={styles.inner}>
+                        <Text>Box 2</Text>
+                    </View>
+                </View>
             </View>
+
         );
     }
 }
 
 const styles = StyleSheet.create({
     container: {
+        width: '100%',
+        height: '85%',
+        padding: 5,
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+    box: {
+        width: '50%',
+        height: '50%',
+        padding: 5
+    },
+    inner: {
         flex: 1,
+        backgroundColor: '#b8d8de',
         alignItems: 'center',
-        justifyContent: 'center'
-
+        justifyContent: 'center',
     }
 });
--- src/Header.js
+++ src/Header.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-
 export default class Header extends React.Component {
 
     render() {
         return (
             <View style={styles.header}>
-
+                <Text>Header Component</Text>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
+    header: {
         width: '100%',
         height: '15%',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f26f85',
-
     }
 });
